9d7ff0c34fe09cf49b66ff2b8ceec2c3a58866a7
Fabien C. Y. Benureau <fabien.benureau+git@gmail.com>
2017-07-13 14:21:17 +0200

    Add provenance data.

--- r5/__init__.py
+++ r5/__init__.py
@@ -1,15 +1,18 @@
+import argparse
+import platform
+import json
+from datetime import datetime
+
+from .walker import walk
+
 # versioneer
 from . import _version
 __version__ = _version.get_versions()['version']
 
 
-from .walker import walk
-
-
 def main():
     """Entry point for the `r5` console script"""
     # handling command line arguments
-    import argparse
     parser = argparse.ArgumentParser("Random walk")
     parser.add_argument('--seed', type=int, default=1,
                         help='seed for random number generator ')
@@ -17,12 +20,23 @@ def main():
                         help='number of step(s) to walk')
     args = parser.parse_args()
 
+    # get provenance
+    provenance = {'python'   : {'implementation': platform.python_implementation(),
+                                'version'       : platform.python_version_tuple(),
+                                'compiler'      : platform.python_compiler(),
+                                'branch'        : platform.python_branch(),
+                                'revision'      : platform.python_revision()},
+                  'platform' : platform.platform(),
+                  'git_info' : _version.get_versions(),
+                  'timestamp': datetime.utcnow().isoformat()+'Z',  # Z stands for UTC
+                 }
+
     # random walk for n steps
     x = walk(args.n, seed=args.seed)
 
     # display & save results
-    import json
-    results = {'seed': args.seed, 'steps': args.n, 'walk': x}
+    results = {'seed': args.seed, 'steps': args.n, 'walk': x,
+               'provenance': provenance}
     for key in ['seed', 'steps', 'walk']:
         print('{}: {}'.format(key, results[key]))
 
