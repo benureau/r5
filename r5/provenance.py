70ffd1d28a343f70da9a1ad66d3fc4cbeb1c3424
Fabien C. Y. Benureau <fabien.benureau+git@gmail.com>
2017-08-01 00:57:03 +0200

    add git diff to provenance data

--- r5/provenance.py
+++ r5/provenance.py
@@ -1,3 +1,4 @@
+import os
 import platform
 from datetime import datetime
 
@@ -13,5 +14,18 @@ def provenance():
                                 'revision': platform.python_revision()},
             'platform' : platform.platform(),
             'git_info' : _version.get_versions(),
+            'git_diff' : git_diff(),
             'timestamp': datetime.utcnow().isoformat()+'Z',  # Z stands for UTC
            }
+
+def git_diff():
+    """Try to retrieve the git diff."""
+    import git
+    try: # if the package is installed or out of it git repository, this will fail.
+        here = os.path.abspath(os.path.dirname(__file__))
+        repo = git.Repo(here, search_parent_directories=True)
+        diff = repo.git.diff(repo.head.commit.tree)
+    except git.exc.InvalidGitRepositoryError as e:
+        # FIXME detect if code is dirty
+        diff = 'error: could not retrieve diff from git ({}).'.format(e)
+    return diff
